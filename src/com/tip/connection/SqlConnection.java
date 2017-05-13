032e187983e4808cc1287f39d4a276c9073752ae
Nutzu <ionutz96.alexx@gmail.com>
2017-05-14 01:50:01 +0300

    BD Conn Close

--- src/com/tip/connection/SqlConnection.java
+++ src/com/tip/connection/SqlConnection.java
@@ -107,6 +107,14 @@ public class SqlConnection {
 				return null;
 			}
 		}
+		//		public static boolean close(Connection c){
+//			try{
+//				c.close();
+//				return true;
+//			}catch(Exception e){
+//				return false;
+//			}
+//		}
 		
 		
 }
